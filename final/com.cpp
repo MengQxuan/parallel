1558c010950e94674f250eebbffeb2b54f6fa9d3
没情绪 <3264330095@qq.com>
2025-07-01 18:14:33 +0800

    期末研究报告初始化

---
+++ final/com.cpp
@@ -0,0 +1,95 @@
+// 串行
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+using namespace std::chrono;
+
+void initialize_matrix(double **A, double *b, int n)
+{
+    srand(time(0));
+    for (int i = 0; i < n; ++i)
+    {
+        fill(A[i], A[i] + n, 0.0);
+        A[i][i] = rand() % 100 + 1000; // 对角线元素大值，避免除0
+        for (int j = i + 1; j < n; ++j)
+        {
+            A[i][j] = rand() % 100 + 1;
+        }
+        b[i] = rand() % 100 + 1;
+    }
+
+    // 随机线性组合，保持可解
+    for (int k = 0; k < n / 2; ++k)
+    {
+        int row1 = rand() % n;
+        int row2 = rand() % n;
+        double factor = (rand() % 100) / 100.0;
+        for (int j = 0; j < n; ++j)
+        {
+            A[row1][j] += factor * A[row2][j];
+        }
+        b[row1] += factor * b[row2];
+    }
+}
+
+void gaussian_elimination(double **A, double *b, double *x, int n)
+{
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = k + 1; i < n; i++)
+        {
+            double factor = A[i][k] / A[k][k];
+            for (int j = k + 1; j < n; ++j)
+            {
+                A[i][j] -= factor * A[k][j];
+            }
+            b[i] -= factor * b[k];
+        }
+    }
+
+    x[n - 1] = b[n - 1] / A[n - 1][n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        double sum = b[i];
+        for (int j = i + 1; j < n; j++)
+        {
+            sum -= A[i][j] * x[j];
+        }
+        x[i] = sum / A[i][i];
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "输入矩阵维度: ";
+    cin >> n;
+
+    double **A = new double *[n];
+    for (int i = 0; i < n; ++i)
+    {
+        A[i] = new double[n];
+    }
+    double *b = new double[n];
+    double *x = new double[n];
+
+    initialize_matrix(A, b, n);
+
+    auto start = high_resolution_clock::now();
+    gaussian_elimination(A, b, x, n);
+    auto end = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "串行高斯消元完成，用时：" << duration.count() << "us" << endl;
+
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] A[i];
+    }
+    delete[] A;
+    delete[] b;
+    delete[] x;
+    return 0;
+}
